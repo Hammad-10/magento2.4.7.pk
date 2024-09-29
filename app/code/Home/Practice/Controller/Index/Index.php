a503677fa66fc22e5c8574ed42404c3484e7de24
Hammad-10 <idreeshammad579@gmail.com>
2024-09-29 21:48:30 +0500

    created a new practice module

---
+++ app/code/Home/Practice/Controller/Index/Index.php
@@ -0,0 +1,26 @@
+<?php
+namespace Home\Practice\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+
+        // echo 'New module is here';
+
+        // Render the page
+        return $this->resultPageFactory->create();
+    }
+}
