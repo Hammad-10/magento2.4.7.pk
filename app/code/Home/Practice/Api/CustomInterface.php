ab8a9e638a1c31db03581acb854084d9b9038f32
Hammad-10 <idreeshammad579@gmail.com>
2024-09-30 15:07:10 +0500

    created custom rest api

---
+++ app/code/Home/Practice/Api/CustomInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Practice\Api; 
+interface CustomInterface
+{
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+ 
+    public function getData($value);
+}
